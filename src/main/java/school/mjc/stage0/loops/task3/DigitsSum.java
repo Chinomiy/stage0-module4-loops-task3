53e7d8285452985a3810da0de5ebb9ab762b712e
Chinomiy <maruishiwo@gmail.com>
2023-10-15 21:06:48 +0300

    v1

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0;
+        if (t == 0) {
+            System.out.println(0);
+            return;
+        }
+        while(t != 0) {
+            if (t < 0) {
+             t *= -1;
+            }
+            int digit = t % 10;
+            sum += digit;
+            t /=10;
+        }
+        System.out.println(sum);
     }
 }
